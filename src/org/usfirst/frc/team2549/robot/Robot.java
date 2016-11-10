2d323d5ccdf8269f309a8c6b78c56372e3a7b17b
2549-Elitebook <millerbots@gmail.com>
2016-11-10 17:39:51 -0600

    testing commit

--- src/org/usfirst/frc/team2549/robot/Robot.java
+++ src/org/usfirst/frc/team2549/robot/Robot.java
@@ -14,7 +14,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
  * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
+ * creating this project, you must also update the manifest file in the resource  aldkjfas;ldk=fjasd;4l=kfj
  * directory.
  */
 public class Robot extends IterativeRobot {
