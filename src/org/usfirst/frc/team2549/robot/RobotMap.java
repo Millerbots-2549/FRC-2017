64b693e78df818d4e1ec9866fde5ee1de052c1da
mrsna <mrsna@DESKTOP-NJK6PQE>
2017-01-16 20:31:05 -0600

    commit1

--- src/org/usfirst/frc/team2549/robot/RobotMap.java
+++ src/org/usfirst/frc/team2549/robot/RobotMap.java
@@ -2,23 +2,15 @@ package org.usfirst.frc.team2549.robot;
 
 import edu.wpi.first.wpilibj.Talon;
 
-/**
- * The RobotMap is a mapping from the ports sensors and actuators are wired into
- * to a variable name. This provides flexibility changing wiring, makes checking
- * the wiring easier and significantly reduces the number of magic numbers
- * floating around.
- */
-public class RobotMap {
-    // For example to map the left and right motors, you could define the
-    // following variables to use with your drivetrain subsystem.
-     public static int motor1 = 1;
-     public static int motor2 = 2;
+public class RobotMap
+{
+     public static int motorLF	= 0;
+     public static int motorLB	= 1;
+     public static int motorRF	= 2;
+     public static int motorRB	= 3;
      
-     Talon talon1 = new Talon(motor1);
-     Talon talon2 = new Talon(motor2);
-    
-    // If you are using multiple modules, make sure to define both the port
-    // number and the module. For example you with a rangefinder:
-    // public static int rangefinderPort = 1;
-    // public static int rangefinderModule = 1;
+     public static final Talon talonLF = new Talon(motorLF);
+     public static final Talon talonLB = new Talon(motorLB);
+     public static final Talon talonRF = new Talon(motorRF);
+     public static final Talon talonRB = new Talon(motorRB);
 }
